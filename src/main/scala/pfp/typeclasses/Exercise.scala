5581f1d3d568d54fa22ac44f50a8cf33be78a086
squid314 <squid314@users.noreply.github.com>
2017-05-25 11:21:54 -0600

    how type classes work in Scala
    - TODO try to rebuild all of this in Haskell

--- src/main/scala/pfp/typeclasses/Exercise.scala
+++ src/main/scala/pfp/typeclasses/Exercise.scala
@@ -4,11 +4,40 @@ object Exercise {
  
   /** Typeclasses */
   // 1. Implement Equalz[A] typeclass that has method def eq(a1: A, a2: A): Boolean
+  trait Equalz[A] { def equ(a1: A, a2: A): Boolean }
 
   // 2. Write EqualzOps & Equalz companion object with apply method
+  object Equalz {
+    def apply[A : Equalz]: Equalz[A] = implicitly[Equalz[A]]
+
+    implicit class EqualzOps[A : Equalz](a: A) { def equ(a2: A): Boolean = Equalz[A].equ(a,a2) }
+  }
 
   // 3. Write instance for String and User
   case class User(login: String)
+  implicit val StringEqualz: Equalz[String] = new Equalz[String] { def equ(a1:String,a2:String) = a1 == a2 }
+  import Equalz._
+  implicit val UserEqualz: Equalz[User] = new Equalz[User] { def equ(u1:User,u2:User) = (u1.login).equ(u2.login) }
 
   // 4. if A & B have instance for Equalz, can u write generic instance for Tuple (A, B)
+  // because the types inside of the tuple are not known, we can't use an implicit val and must use an implicit def to generate the value
+  implicit def Tuple2Equalz[A : Equalz, B : Equalz]: Equalz[(A, B)] =
+    new Equalz[(A, B)] { def equ(t1: (A,B), t2: (A,B)) = t1._1.equ(t2._1) && t1._2.equ(t2._2) }
+  // btw, we could do the same thing if we only had one type parameter we cared about
+//  implicit def Tuple2FirstEqualz[A : Equalz, B]: Equalz[(A,B)] =
+//    new Equalz[(A, B)] { def equ(t1: (A,B), t2: (A,B)) = t1._1.equ(t2._1) /*&& t1._2.equ(t2._2)*/ }
+  // well... that's not working
+
+  def _main(args: Array[String]) {
+    val weirdo1: (String, User) = ("blah", User("asdf"))
+    val weirdo2: (String, User) = ("blah", User("asdf"))
+    val weirdo3: (String, User) = ("asdf", User("asdf"))
+    val weirdo4: (String, User) = ("asdf", User("blah"))
+    println(weirdo1 equ weirdo2)
+    println(weirdo1 equ weirdo3)
+    println(weirdo1 equ weirdo4)
+    println(weirdo2 equ weirdo3)
+    println(weirdo2 equ weirdo4)
+    println(weirdo3 equ weirdo4)
+  }
 }
--- src/main/scala/pfp/typeclasses/Typeclasses.scala
+++ src/main/scala/pfp/typeclasses/Typeclasses.scala
@@ -2,7 +2,35 @@ package pfp.intro
 
 object Typeclasses {
 
+
   class User(val firstName: String, val lastName: String)
 
+  def hello[A](a: A)(implicit sh: Show[A]): String = "hi " + sh.show(a)
+
+  trait Show[A] { def show(a: A): String }
+  // in Haskell terminology, type A is now constrained by the type class Show
+  def hello1[A : Show](a: A): String = "hi " + implicitly[Show[A]].show(a)
+
+  object Show {
+    def apply[A : Show]: Show[A] = implicitly[Show[A]]
+
+    implicit class ShowOps[A : Show](a: A) { def show: String = Show[A].show(a) }
+  }
+  def hello2[A : Show](a: A): String = "hi " + Show[A].show(a)
+  def hello3[A : Show](a: A): String = {
+    import Show._ // pull in that ShowOps class
+    "hi " + a.show // compiler automagically wraps A with ShowOps and then we can call a.show
+  }
+
+  def _main(args: Array[String]) {
+    implicit val UserShow: Show[User] = new Show[User] {
+      def show(user: User) = user.firstName + " " + user.lastName
+    }
+    val user: User = new User("John", "The Camera Man")
+    println(hello(user))
+    println(hello1(user))
+    println(hello2(user))
+    println(hello3(user))
+  }
 }
 
